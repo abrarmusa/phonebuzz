264117f0e44dcae05d53a7a84cbf9b67fff0348f
abrarmusa <abrar.musa.89@gmail.com>
2015-11-19 09:44:19 -0800

    removed reference to user.js in app.js

---
+++ public/javascripts/fizzbuzz.js
@@ -0,0 +1,23 @@
+var fizzbuzz = function(n) {
+  
+  var str = " ";
+  for (i = 1; i <=n; i++){
+    if (i % 3 == 0 && i % 5 == 0) {
+        str += "Fizzbuzz ";
+    }
+    else if (i % 3 == 0) {
+        str += "Fizz ";
+    }
+    else if (i % 5 == 0) {
+        str += "Buzz ";
+    }
+    else {
+        str += i + ' ';
+    }
+  }
+
+
+  return str;
+};
+
+module.exports = fizzbuzz;
